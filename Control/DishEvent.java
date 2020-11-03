08e00ea5ebdaa895d5db04706b936c1a734dc001
PolinaBrusova <64200729+PolinaBrusova@users.noreply.github.com>
2020-11-03 09:49:07 +0300

    Прикреплена контрольная работа за 03.10.2020

---
+++ Control/DishEvent.java
@@ -0,0 +1,5 @@
+package Control;
+
+public interface DishEvent {
+    void note(String dish, String table);
+}
---
+++ Control/Place.java
@@ -0,0 +1,11 @@
+package Control;
+
+import java.util.ArrayList;
+
+public class Place {
+    public static ArrayList<String> places =  new ArrayList<>();
+
+    public Place(){
+        //класс содержит в себе список занятых на данный момент столиков
+    }
+}
